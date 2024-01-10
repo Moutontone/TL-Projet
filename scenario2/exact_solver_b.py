fc93a8943ff91aed1b4d9b32aaca8058b246e6fe
JoachimMakowski <kajt1999@o2.pl>
2024-01-10 17:02:19 +0100

    working both exact solvers

--- scenario2/exact_solver_b.py
+++ scenario2/exact_solver_b.py
@@ -6,10 +6,6 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from python.instance import Instance
 from python.verifSolution import *
 
-###############################################################
-################### Optimization arguments ####################
-###############################################################
-
 argc = len(sys.argv)-1
 
 optMode = '-O2'
@@ -34,22 +30,61 @@ if argc >= 1:
 
 # Getting data from corresponding data folder
 instance = Instance(datapath)
+if len(instance.demands) == 1:
+	instance.demands = instance.demands[0] # to not iterate over days, since we have only one day to test
 
-###############################################################
-################## MIP Model of the Problem ###################
-###############################################################
+list_of_demand_indices = [0] #index of each node
+
+list_of_demands = [0]# demand for the given index
+print('demands:', instance.demands)
+print('distance:')
+for x in instance.distanceMatrix:
+	print(x)
+for client in range(len(instance.clients)):
+	print('client id', instance.clients[client])
+	print(client, instance.demands[client])
+	for farmer in range(len(instance.farmers)):
+		if instance.demands[client][farmer] > 0:
+			list_of_demand_indices.append(instance.farmers[farmer])
+			list_of_demand_indices.append(instance.clients[client])
+
+			list_of_demands.append(instance.demands[client][farmer])
+			list_of_demands.append(-instance.demands[client][farmer])
+print(list_of_demand_indices)
+
+tmp_dist_matrix = [[0]]
+#tmp_demand_matrix = [[]]
+#tmp_demand_matrix[0] = [0 for _ in range(len(list_of_demand_indices))]
+
+for index in list_of_demand_indices[1:]:
+	#print(index)
+	for x in range(len(tmp_dist_matrix)):
+		tmp_dist_matrix[x].append(instance.distanceMatrix[list_of_demand_indices[x]][index])
+	tmp_list = [row[-1] for row in tmp_dist_matrix] + [0]
+	tmp_dist_matrix.append(tmp_list)
+
+	#print(tmp_list)
+
+'''for i in range(1,len(list_of_demand_indices),2):
+	tmp_demand_matrix.append([0 for _ in range(len(list_of_demand_indices))])
+	tmp_demand_matrix.append([0 for _ in range(len(list_of_demand_indices))])
+	tmp_demand_matrix[i][i+1] = instance.demands[list_of_demand_indices[i+1]-1-instance.nbFarmers][list_of_demand_indices[i]-1]
+	tmp_demand_matrix[i+1][i] = -instance.demands[list_of_demand_indices[i+1]-1-instance.nbFarmers][list_of_demand_indices[i]-1]
+'''
+#print(tmp_demand_matrix)
+print(tmp_dist_matrix)
 
 m = Model()
 m.verbose = solverVerbose
 
 all_locations = [i for i in range(instance.nbFarmers+instance.nbClients+1)]
 
-x = [[m.add_var(var_type=BINARY) for _ in all_locations] for _ in all_locations]
-y = [[m.add_var(var_type=BINARY) for _ in all_locations] for _ in all_locations]
-z = [m.add_var(var_type=CONTINUOUS) for _ in all_locations]
-load = [[m.add_var(var_type=INTEGER) for _ in all_locations] for _ in all_locations]
+x = [[m.add_var(var_type=BINARY) for _ in list_of_demand_indices] for _ in list_of_demand_indices]
+y = [[m.add_var(var_type=BINARY) for _ in list_of_demand_indices] for _ in list_of_demand_indices]
+z = [m.add_var(var_type=CONTINUOUS) for _ in list_of_demand_indices]
+load = [[m.add_var(var_type=INTEGER) for _ in list_of_demand_indices] for _ in list_of_demand_indices]
 
-for i in all_locations: #without depot
+for i in range(len(list_of_demand_indices)): #without depot
     #m += load[0][i] == 0                                          #load_1
     #m += load[i][0] == 0                                          #load_2
 
@@ -57,13 +92,13 @@ for i in all_locations: #without depot
     #m += xsum(x[j][i] for j in all_locations) <= 1
     
   
-    for j in all_locations:
+    for j in range(len(list_of_demand_indices)):
         m += y[i][j]+y[j][i] <= 1                                   #precedence_1
         #m += x[i][j] <= y[i][j]                                     #precedence_3
 
         m += load[i][j] <= instance.capacity * x[i][j]                       #load_4
     
-        for k in all_locations:
+        for k in range(len(list_of_demand_indices)):
             m += x[j][i]+y[i][j]+y[j][k]+y[k][i] <= 2                 #precedence_2
 
 #for order in instance.demandsFarmersClients:
@@ -72,25 +107,24 @@ for i in all_locations: #without depot
 #m += xsum(x[0][j] for j in all_locations[1:]) == 1 #depot_1
 #m += xsum(x[i][0] for i in all_locations[1:]) == 1 #depot_2
 
-for i in instance.farmers:
-    for j in instance.clients:
-        m += instance.capacity*y[i][j] >= instance.demands[instance.day][j-instance.nbFarmers-1][i-1]    #precedence_4
+#for i in instance.farmers:
+    #for j in instance.clients:
+for i in range(1,len(list_of_demand_indices),2):
+    m += y[i][i+1] == 1    #precedence_4
 
-for j in instance.farmers:     
-    m += xsum(load[i][j] for i in all_locations) == xsum(load[j][k] for k in all_locations) - xsum(instance.demands[instance.day][k-instance.nbFarmers-1][j-1] for k in instance.clients) #load_3a
+for j in range(len(list_of_demand_indices)):     
+    m += xsum(load[i][j] for i in range(len(list_of_demand_indices))) == xsum(load[j][k] for k in range(len(list_of_demand_indices))) - list_of_demands[j] #load_3
     
 
-for j in instance.clients:
-    m += xsum(load[i][j] for i in all_locations) - xsum(instance.demands[instance.day][j-instance.nbFarmers-1][i-1] for i in instance.farmers) == xsum(load[j][k] for k in all_locations) #load_3b
 
-for i in all_locations:
+for i in range(len(list_of_demand_indices)):
     m += x[i][i] == 0 #flow_2
-    m += xsum(x[i][j] for j in all_locations) == xsum(x[k][i] for k in all_locations) #flow_1
-    m += xsum(x[i][j] for j in all_locations) == 1 #flow_1b
+    m += xsum(x[i][j] for j in range(len(list_of_demand_indices))) == xsum(x[k][i] for k in range(len(list_of_demand_indices)))
+    m += xsum(x[j][i] for j in range(len(list_of_demand_indices))) == 1
     m += load[0][i] == 0                                          #load_1
-    m += load[i][0] == 0                                          #load_2
-    for j in all_locations[1:]:
-        m += x[j][i] <= y[j][i]                                   #precedence_3
+    m += load[i][0] == 0 
+    for j in range(1,len(list_of_demand_indices)):
+        m += x[j][i] <= y[j][i]
     #m += load[i][i] == 0
 '''
 if optMode == '-O0':
@@ -99,12 +133,12 @@ if optMode == '-O0':
     for it in combinations(instance.farmers, k):
       m += xsum(xsum(xsum(x[t][i][j] for i in it) for j in it)for t in newtours) <= k-1
 if optMode == '-O1' or optMode == '-O2':'''
-for i in all_locations[1:]:
-    for j in all_locations:
-        m += z[i]-(all_locations[-1]+1)*x[i][j] >= z[j] - all_locations[-1]
-        m += z[i]-(all_locations[-1]+1)*y[i][j] >= z[j] - all_locations[-1]
+for i in range(1,len(list_of_demand_indices)):
+    for j in range(len(list_of_demand_indices)):
+        m += z[i]-(len(list_of_demand_indices)+1)*x[i][j] >= z[j] - len(list_of_demand_indices)
+        m += z[i]-(len(list_of_demand_indices)+1)*y[i][j] >= z[j] - len(list_of_demand_indices)
 
-m.objective = minimize(xsum(xsum(x[i][j]*instance.dist(i, j) for i in all_locations)for j in all_locations))
+m.objective = minimize(xsum(xsum(x[i][j]*tmp_dist_matrix[i][j] for i in range(len(list_of_demand_indices)))for j in range(len(list_of_demand_indices))))
 
 # Giving the solution of the greedy heuristic as a starting point TODO: give a non-partial solution
 #m.start = [(x[greedyStart[k][0]][greedyStart[k][1]][greedyStart[k][2]], 1.0) for k in range(len(greedyStart))]
@@ -115,15 +149,15 @@ m.objective = minimize(xsum(xsum(x[i][j]*instance.dist(i, j) for i in all_locati
 ###############################################################
 
 if optSolve == True:
-    solution = [[0 for _ in all_locations] for _ in all_locations]
-    status = m.optimize(max_seconds=300)
+    solution = [[0 for _ in list_of_demand_indices] for _ in list_of_demand_indices]
+    status = m.optimize(max_seconds=60)
     if status == OptimizationStatus.OPTIMAL:
         print('\nAn optimal solution was found with an objective value of {}'.format(m.objective_value))
 
         if optVerbose == True:
             print('x')
-        for i in all_locations:
-            for j in all_locations:
+        for i in range(len(list_of_demand_indices)):
+            for j in range(len(list_of_demand_indices)):
                 solution[i][j] = x[i][j].xi(0)
                 if optVerbose == True:
                     print(int(x[i][j].xi(0)),end='  ')
@@ -131,23 +165,23 @@ if optSolve == True:
                 print('')
         if optVerbose == True:
             print('y')
-        for i in all_locations:
-            for j in all_locations:
+        for i in range(len(list_of_demand_indices)):
+            for j in range(len(list_of_demand_indices)):
                 if optVerbose == True:
                     print(int(y[i][j].xi(0)),end='  ')
             if optVerbose == True:
                 print('')
         if optVerbose == True:
             print('load')
-        for i in all_locations:
-            for j in all_locations:
+        for i in range(len(list_of_demand_indices)):
+            for j in range(len(list_of_demand_indices)):
                 if optVerbose == True:
                     print(int(load[i][j].xi(0)),end='  ')
             if optVerbose == True:
                 print('')
         if optVerbose == True:
             print('z')
-        for j in all_locations:
+        for j in range(len(list_of_demand_indices)):
             if optVerbose == True:
                 print(float(z[j].xi(0)),end='  ')
         if optVerbose == True:
@@ -157,14 +191,17 @@ if optSolve == True:
             the_path = [0]
             curr_index = 0
             while True:
-                for i in all_locations:
+                for i in range(len(list_of_demand_indices)):
                     if int(x[curr_index][i].xi(0)) == 1:
-                        the_path.append(i)
+                        the_path.append(list_of_demand_indices[i])
                         curr_index = i
                         break
                 if curr_index == 0:
                     break
+            print(list_of_demand_indices)
             print(the_path)
+                        
+					
         if optVerbose == True:
             print('\n  #############################################\n')
     elif status == OptimizationStatus.FEASIBLE:
